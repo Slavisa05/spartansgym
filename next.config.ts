e580a2d6cfb673bdc7dafa98899ae8fa14805587
Slavisa <slavisaarsenijevic05@gmail.com>
2026-05-06 22:03:48 +0200

    config

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* OVO SE BRISE KADA BUDE IMAO SVOJ DOMEN */
-  basePath: '/gym',
-  async redirects() {
-    return []
-  },
+
 };
 
 export default nextConfig;
